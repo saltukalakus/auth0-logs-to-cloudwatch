6b24e5d0110969c9c00d397f91c7239659bc8e7a
Brian Beck <bbeck@DGBrianBeck.local>
2017-10-05 08:33:51 -0600

    renamed to cloudwatch

--- server/index.js
+++ server/index.js
@@ -46,13 +46,13 @@ module.exports = (configProvider, storageProvider) => {
   // Configure routes.
   app.use(expressTools.routes.dashboardAdmins({
     secret: config('EXTENSION_SECRET'),
-    audience: 'urn:logs-to-logstash',
+    audience: 'urn:logs-to-cloudwatch',
     rta: config('AUTH0_RTA').replace('https://', ''),
     domain: config('AUTH0_DOMAIN'),
     baseUrl: config('PUBLIC_WT_URL') || config('WT_URL'),
-    clientName: 'Logs to Logstash',
+    clientName: 'Logs to CloudWatch',
     urlPrefix: '',
-    sessionStorageKey: 'logs-to-logstash:apiToken'
+    sessionStorageKey: 'logs-to-cloudwatch:apiToken'
   }));
 
   app.use('/app', Express.static(path.join(__dirname, '../dist')));
